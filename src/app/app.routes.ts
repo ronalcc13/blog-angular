636bd731e0d51ba621eb39929b0a123908700c53
ronalcc13 <ronaldocc13@gmail.com>
2025-09-15 22:52:23 -0400

    Feat: Layout e Home y About

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            {
+                path: '',
+                component: Home,
+                title: 'Home'
+            },
+            {
+                path: 'about',
+                component: About,
+                title: 'About'
+            }
+        ]
+    }
+];
